566520eee8f9c7dc9fa99745a0e71d8d5f8efa37
fisher60 <wakefollower@gmail.com>
2021-01-24 23:49:28 -0800

    remove intro title, center category title

--- portfolio/src/components/home.jsx
+++ portfolio/src/components/home.jsx
@@ -8,9 +8,6 @@ import programmingImg from "./programming.jpg";
 export default function Home(){
     return(
         <div className="home-content">
-            <div className="home-title">
-                <h1>Intro</h1>
-            </div>
                 <div className="categories-container">
                 <Category title="Aviation" navLink="aviation" backgroundImg={aviationImg}/>
                 <Category title="Personal" navLink="personal" backgroundImg={personalImg}/>
