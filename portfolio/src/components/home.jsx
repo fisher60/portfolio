9efdccc9ec59132ef3216a3ec92d58d0859c5169
fisher60 <wakefollower@gmail.com>
2021-01-28 21:10:16 -0800

    add profile picture placeholder

--- portfolio/src/components/home.jsx
+++ portfolio/src/components/home.jsx
@@ -19,7 +19,7 @@ export default function Home(){
                 <Category title="Programming" navLink="programming" backgroundImg={programmingImg}/>
             </div>
 
-            <Outro />
+            {/* <Outro /> */}
         </div>
     )
 }
--- portfolio/src/components/intro.jsx
+++ portfolio/src/components/intro.jsx
@@ -3,11 +3,15 @@ import "./intro.css";
 export default function Intro(){
     return(
         <div className="intro-wrapper">
-            <h1>Hello,</h1>
-            <h2 className="title-mid">I am Kyler</h2>
-            <h2 className="title-bot">I program and fly airplanes.</h2>
+            <div className="intro-text">
+                <h1>Hello,</h1>
+                <h2 className="title-mid">I am Kyler</h2>
+                <h2 className="title-bot">I program and fly airplanes.</h2>
+            </div>
 
-            
+            <div className="profile-picture">
+                <span>Placeholder</span>
+            </div>
         </div>
     )
 }
