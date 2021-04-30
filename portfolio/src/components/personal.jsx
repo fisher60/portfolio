7e6a7a4b8aeef2a650908b9a8bbbb540f323def6
Kyler Roloff <kyler.roloff@gmail.com>
2021-04-29 18:25:34 -0700

    add sample info to about pages, tweak home styling

--- portfolio/src/components/personal.jsx
+++ portfolio/src/components/personal.jsx
@@ -1,7 +1,19 @@
+import "./info.css";
+
 export default function Personal(){
     return(
-        <div>
-            <span>Personal</span>
+        <div className="info-container">
+            <h1 className="info-title">Personal</h1>
+            <hr/>
+            <p className="info-about">
+                I love my dog. Ella is, quite literally, more important to me than most things.
+            </p>
+            <p className="info-about">
+                I also play video games sometimes.
+            </p>
+            <p className="info-about">
+                This is all you need to know about me.
+            </p>
         </div>
     )
 }
--- portfolio/src/components/programming.jsx
+++ portfolio/src/components/programming.jsx
@@ -1,7 +1,24 @@
+import {GITHUB_URL} from "./constants";
+import "./info.css";
+
 export default function Programming(){
     return(
-        <div>
-            <span>Programming</span>
+        <div className="info-container">
+            <h1 className="info-title">Programming</h1>
+            <hr/>
+            <p className="info-about">
+                I am a programming hobbyist with large interests in Python and Aviation. I love learning and have played
+                with a handful of languages, but Python has always been my favorite.
+            </p>
+            <p className="info-about">
+                Most of my recent experience in Python has been with backend web development and leading various
+                projects. Many of my passion projects involve combining discord bots with backend web services and APIs.
+                My favorite stack tends to be Docker + Django + REST, however I have also enjoyed playing around with
+                FastAPI as well as GraphQL.
+            </p>
+            <h3>Most of my public projects can be viewed on my
+                <a className="info-link" href={GITHUB_URL} target="_blank" rel="noopener noreferrer"> Github</a>
+            </h3>
         </div>
     )
 }
