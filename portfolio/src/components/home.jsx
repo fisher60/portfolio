1ea7ee2f592dc365989cc37261f69008933d98f3
fisher60 <wakefollower@gmail.com>
2023-05-16 23:35:01 -0500

    refactor home page for tailwind, replace info banner
    I have refactored to use tailwind, changed the color scheme to use orange and added some preliminary mobile responsiveness.

--- portfolio/src/components/home.jsx
+++ portfolio/src/components/home.jsx
@@ -1,6 +1,3 @@
-import "./home.css";
-
-import Intro from "./intro.jsx";
 import Category from "./category.jsx";
 
 import aviationImg from "./aviation.jpg";
@@ -9,10 +6,14 @@ import programmingImg from "./programming.jpg";
 
 export default function Home(){
     return(
-        <div className="home-content">
-            <Intro />
+        <div>
+            <div className="flex flex-col">
+                <p className="text-6xl ml-16">Hello,</p>
+                <p className="text-4xl ml-32 pt-2">I am <span className="text-orange-400">Kyler</span></p>
+                <p className="text-2xl ml-40 pt-2">I program and fly airplanes.</p>
+            </div>
 
-            <div className="categories-container">
+            <div className="flex flex-col md:flex-row items-center justify-center pt-20">
                 <Category title="Aviation" navLink="aviation" backgroundImg={aviationImg}/>
                 <Category title="Personal" navLink="personal" backgroundImg={personalImg}/>
                 <Category title="Programming" navLink="programming" backgroundImg={programmingImg}/>
