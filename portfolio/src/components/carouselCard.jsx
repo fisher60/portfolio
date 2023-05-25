fd531354609f34f3c57c024ab79eb847e5947a49
Kyler Roloff <wakefollower@gmail.com>
2023-05-24 20:23:39 -0500

    explicitly set bg-white on cards
    bg-base-100 leaves the background color ambiguous. This was causing the background color to change depending on device/browser

--- portfolio/src/components/carouselCard.jsx
+++ portfolio/src/components/carouselCard.jsx
@@ -1,6 +1,6 @@
 export default function CarouselCard(props) {
     return (
-        <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card bg-white w-96 shadow-xl">
             <div className="card-body">
                 <h2 className="card-title">{props.title}</h2>
                 <p>{props.description}</p>
